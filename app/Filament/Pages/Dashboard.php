a652e70b2825e7df8e6eae0cdd17679759ad0098
webard <b.gajda@webard.me>
2026-03-09 18:35:03 +0100

    fix dashboard register

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Pages;
+
+final class Dashboard extends \Filament\Pages\Dashboard
+{
+     protected static bool $shouldRegisterNavigation = false;
+}
